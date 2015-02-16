99caedd08075d7bacfffe6387ed4c03ed9b54062
whusted <whusted@lion.lmu.edu>
2015-02-15 17:51:43 -0800

    Beginning of api & server

---
+++ InfiniteMoment/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  InfiniteMoment
+//
+//  Created by Willy Husted on 1/27/15.
+//  Copyright (c) 2015 Line40. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
