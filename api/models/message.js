06269d0590706e0c05e49592dfcf009ecf68fb90
whusted <whusted@lion.lmu.edu>
2015-03-12 17:05:15 -0700

    Rough sketch of Message schema

---
+++ api/models/message.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    autoIncrement = require('mongoose-auto-increment');
+
+autoIncrement.initialize(mongoose);
+
+var messageSchema = new Schema({
+  id: String,
+  content: {type: String, required: true },
+  creationDate: { type: Date, required: true, default: Date.now },
+  deliveryDate: { type: Date, required: true, default: Date.now }
+});
+
+messageSchema.plugin(autoIncrement.plugin, 'Message');
+var Message = mongoose.model('Message', messageSchema, 'Messages');
+
+exports.Message = Message;  
