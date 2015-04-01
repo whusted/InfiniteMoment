c51da319f67bfa0a956e810cd37951fea6306dee
whusted <whusted@lion.lmu.edu>
2015-03-31 18:59:16 -0700

    Added login view controller; reading username and password fields

---
+++ InfiniteMoment/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  InfiniteMoment
+//
+//  Created by Willy Husted on 3/31/15.
+//  Copyright (c) 2015 Infinite Moment LLC. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+
+    @IBAction func login(sender: AnyObject) {
+        let username = usernameField.text
+        let password = passwordField.text
+        
+        println("username: " + username)
+        println("password: " + password)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
