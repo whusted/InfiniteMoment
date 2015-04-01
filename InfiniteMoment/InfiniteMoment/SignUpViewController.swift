de3bf036345226fa30022c5a81be1a1ed5cbbe43
whusted <whusted@lion.lmu.edu>
2015-04-01 09:25:06 -0700

    Sign up controller added

---
+++ InfiniteMoment/InfiniteMoment/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  InfiniteMoment
+//
+//  Created by Willy Husted on 4/1/15.
+//  Copyright (c) 2015 Infinite Moment LLC. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
