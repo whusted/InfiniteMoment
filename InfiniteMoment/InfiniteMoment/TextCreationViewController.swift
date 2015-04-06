d4874d2a22d2f66ce3f6ff39950031cdc3115184
whusted <whusted@lion.lmu.edu>
2015-04-05 22:29:00 -0700

    Moment creation content done

--- InfiniteMoment/InfiniteMoment/TextCreationViewController.swift
+++ InfiniteMoment/InfiniteMoment/TextCreationViewController.swift
@@ -13,47 +13,28 @@ import Lockbox
 
 class TextCreationViewController: UIViewController {
     
-    let token = Lockbox.stringForKey("authToken")
-    
     @IBOutlet weak var textBox: UITextField!
     
     override func viewDidLoad() {
         println("In Text Creation")
-        let token = Lockbox.stringForKey("authToken")
         super.viewDidLoad()
     }
     
+        
+    
     @IBAction func nextButton(sender: AnyObject) {
-        
-        let parameters = [
-            "Authorization": token,
-            "conent": textBox.text,
-            "recipients": [],
-            "deliveryDate": nil
-        ]
-        
+    
         if textBox.text.isEmpty {
             var alert = UIAlertController(title: "Oops!", message: "No one wants a blank Moment!", preferredStyle: UIAlertControllerStyle.Alert)
             alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
             self.presentViewController(alert, animated: true, completion: nil)
         } else {
-            Alamofire.request(.POST, "http://localhost:7777/sessions", parameters: loginParameters, encoding: .JSON)
-                .responseJSON {(request, response, json, error) in
-                    var resp = JSON(json!)
-                    if (error != nil) {
-                        NSLog("Error: \(error)")
-                    } else if (resp["error"] != nil) {
-                        var alert = UIAlertController(title: "Oops!", message: resp["message"].string, preferredStyle: UIAlertControllerStyle.Alert)
-                        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-                        self.presentViewController(alert, animated: true, completion: nil)
-                    } else {
-                        println(resp["response"])
-                        let token = Lockbox.setString(resp["token"].string, forKey: "authToken")
-                        self.dismissViewControllerAnimated(true, completion: nil)
-                    }
-                    
-            }
-
+            var parameters = [
+                "content": textBox.text
+            ]
+            println(parameters)
+            // pass parameters to recipients view
+            
         }
     }
     
