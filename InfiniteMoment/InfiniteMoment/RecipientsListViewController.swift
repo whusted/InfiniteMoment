aa847745cfdf4a63a8e1e100a38e29b79c96bada
whusted <whusted@lion.lmu.edu>
2015-04-17 17:17:35 -0700

    Getting friends from db

--- InfiniteMoment/InfiniteMoment/RecipientsListViewController.swift
+++ InfiniteMoment/InfiniteMoment/RecipientsListViewController.swift
@@ -7,15 +7,66 @@
 //
 
 import UIKit
+import Lockbox
+import Alamofire
+import SwiftyJSON
+import Security
 
 class RecipientsListViewController: UITableViewController {
     
     var parameters = Dictionary<String, String>()
+    var friends = Array<JSON>()
+    let fakeFriends = ["thisGuy", "thatGuy"]
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         
         var params = parameters
         println(params)
-        super.viewDidLoad()
+        
+        let token = Lockbox.stringForKey("authToken")
+        println(token)
+        if (token != nil) {
+            Alamofire.Manager.sharedInstance.session.configuration.HTTPAdditionalHeaders = ["Authorization": token]
+        } else {
+            // TODO: Take to login
+        }
+        
+        getFriends("begin", completion: { (result) -> Void in
+            println(self.friends)
+        })
+        
+        
+        
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            println(self.friends.count)
+            return self.friends.count
+        }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            let item = self.friends[indexPath.row].string
+        
+            let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath) as! UITableViewCell
+            cell.textLabel!.text = item
+            return cell
+    }
+    
+
+    func getFriends(input: String, completion: (result: Bool) -> Void) {
+        Alamofire.request(.GET, "http://localhost:7777/friends")
+            .responseJSON { (request, response, json, error) in
+                let json = JSON(json!)
+                if (error != nil) {
+                    NSLog("Error: \(error)")
+                } else if (json["error"] != nil) {
+                    // TODO: handle error
+                } else {
+                    self.friends = json["response"].arrayValue
+                    println(self.friends)
+                    completion(result: true)
+                }
+        }
     }
 }
